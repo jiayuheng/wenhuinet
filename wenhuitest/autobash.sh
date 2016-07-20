0b3dba6a5b2060df8bc3fe455ebb78cc8491c125
jiayuheng <jia_yuheng@126.com>
2016-07-20 04:37:14 -0700

    bash version

---
+++ wenhuitest/autobash.sh
@@ -0,0 +1,30 @@
+bash autowenhui.sh gn7d5_1 128 16 16 0.4688 32 32
+bash autowenhui.sh gn7d5_2 128 16 16 0.4688 32 32
+bash autowenhui.sh gn8_1 128 16 16 0.5 32 32
+bash autowenhui.sh gn8_2 128 16 16 0.5 32 32
+
+
+bash autowenhui.sh LFR1_1 1000 20 50 0.1 30 100
+bash autowenhui.sh LFR1_2 1000 20 50 0.1 30 100
+
+bash autowenhui.sh LFR2_1 1000 20 50 0.2 30 100
+bash autowenhui.sh LFR2_2 1000 20 50 0.2 30 100
+
+bash autowenhui.sh LFR3_1 1000 20 50 0.3 30 100
+bash autowenhui.sh LFR3_2 1000 20 50 0.3 30 100
+
+bash autowenhui.sh LFR4_1 1000 20 50 0.4 30 100
+bash autowenhui.sh LFR4_2 1000 20 50 0.4 30 100
+
+bash autowenhui.sh LFR5_1 1000 20 50 0.5 30 100
+bash autowenhui.sh LFR5_2 1000 20 50 0.5 30 100
+
+bash autowenhui.sh LFR6_1 1000 20 50 0.6 30 100
+bash autowenhui.sh LFR6_2 1000 20 50 0.6 30 100
+
+bash autowenhui.sh LFR6d5_1 1000 20 50 0.65 30 100
+bash autowenhui.sh LFR6d5_2 1000 20 50 0.65 30 100
+
+bash autowenhui.sh LFR7_1 1000 20 50 0.7 30 100
+bash autowenhui.sh LFR7_2 1000 20 50 0.7 30 100
+
